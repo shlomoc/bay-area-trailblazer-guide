cd063a55d171458cfaa92676008a0682fc4b2a60
Steve Cohen <schlomoc@gmail.com>
2025-05-04 11:39:02 -0700

    Update hiking club image URL

--- src/data/hikingClubs.ts
+++ src/data/hikingClubs.ts
@@ -19,7 +19,7 @@ export const hikingClubs: HikingClub[] = [
     location: "San Francisco, Penninsula",
     frequency: "Weekly",
     link: "https://www.meetup.com/mid-peninsula-hiking-activities-group",
-    image: "https://unsplash.com/photos/silhouette-photo-of-three-person-near-tall-trees-xOigCUcFdA8"
+    image: "https://images.unsplash.com/photo-1476611338391-6f395a0ebc7b?q=80&w=400&h=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     id: "sierra-club",
